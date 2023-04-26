6c00a5f7e9e2729fb36860c6c88ee2985477717b
Barbara <barbaraelisabethb@gmail.com>
2023-04-26 14:02:48 -0400

    Resolving Schema Conflicts

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,32 @@ from app import db
 from app.models.book import Book
 from flask import Blueprint, jsonify, make_response, request
 
-books_bp = Blueprint("books", __name__, url_prefix="/books")
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
+    new_book = Book(title=request_body["title"], description=request_body["description"])
 
     db.session.add(new_book)
     db.session.commit()
 
     return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
+
 # class Book:
 #     def __init__(self, id, title, description):
 #         self.id = id
@@ -37,21 +51,7 @@ def handle_books():
 #        if book.id == book_id:
 #            return book
 #
-#    abort(make_response({"message":f"book {book_id} not found"}, 404))
-        
-
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return jsonify(books_response)
+#    abort(make_response({"message":f"book {book_id} not found"}, 404)
 
 # @books_bp.route("/<book_id>", methods=["GET"])
 # def handle_book(book_id):
