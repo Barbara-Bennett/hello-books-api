021ff2d904b5541cafc1784c8bd090c2f99cccc2
Barbara <barbaraelisabethb@gmail.com>
2023-04-26 06:47:27 -0400

    Create a Book Route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
-from os import abort
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
 # class Book:
 #     def __init__(self, id, title, description):
 #         self.id = id
@@ -13,7 +26,6 @@ from flask import Blueprint, jsonify, abort, make_response
 #     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
 # ]
 
-books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 #def validate_book(book_id):
 #    try:
