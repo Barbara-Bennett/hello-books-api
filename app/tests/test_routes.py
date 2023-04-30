4f80587dbb1a36ced8c8dcb39f35877de8bb985c
Barbara <barbaraelisabethb@gmail.com>
2023-04-30 11:13:40 -0400

    Complete 06 - POST /books Test

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -7,7 +7,6 @@ def test_get_all_books_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
-
 def test_get_one_book(client, two_saved_books):
     # Act
     response = client.get("/books/1")
@@ -20,3 +19,15 @@ def test_get_one_book(client, two_saved_books):
         "title": "Ocean Book",
         "description": "watr 4evr"
     }
+
+def test_create_one_book(client):
+    # Act
+    response = client.post("/books", json={
+        "title": "New Book",
+        "description": "The Best!"
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == "Book New Book successfully created"
